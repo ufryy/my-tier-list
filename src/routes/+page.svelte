b89b42b0d47ee8e3a55a83fdc63b459934af093e
m.denardis <m.denardis@reply.it>
2025-01-11 18:30:43 +0100

    feat: tier creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import StagingZone from '$lib/components/tier-list/StagingZone.svelte';
 	import TierList from '$lib/components/tier-list/TierList.svelte';
+	import { Button } from '$lib/components/ui/button';
 	import { getCtxTierList } from '$lib/context';
+	import { Plus } from 'lucide-svelte';
 
 	const tierList = getCtxTierList();
 </script>
 
 <div class="flex flex-col gap-20">
-	<TierList {tierList} />
+	<div>
+		<TierList {tierList} />
+		<Button variant="ghost" class="mt-2" size="sm" onclick={() => tierList.createTier()}>
+			<Plus />
+			Add tier
+		</Button>
+	</div>
 	<StagingZone {tierList} />
 </div>
